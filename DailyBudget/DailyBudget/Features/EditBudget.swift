80a359dee227c13bf5aa084238f0ea2d9d8c33a4
phlippieb <phlippie.bosman@gmail.com>
2024-06-20 13:43:10 +0200

    Add UI for creating or editing budget

---
+++ DailyBudget/DailyBudget/Features/EditBudget.swift
@@ -0,0 +1,106 @@
+import SwiftUI
+
+struct EditBudget: View {
+  enum Mode {
+    case new, edit(Budget)
+  }
+  
+  init(
+    _ mode: Mode,
+    onSave: @escaping (Budget) -> Void = { _ in },
+    onDelete: @escaping () -> Void = {}
+  ) {
+    self.onSave = onSave
+    self.onDelete = onDelete
+    
+    if case .edit(let budget) = mode {
+      self.isEditMode = true
+      self.budget.name = budget.name
+      self.budget.amount = budget.amount
+      self.budget.startDate = budget.startDate
+      self.budget.endDate = budget.endDate
+    } else {
+      self.isEditMode = false
+    }
+  }
+  
+  private let onSave: (Budget) -> Void
+  private let onDelete: () -> Void
+  private let isEditMode: Bool
+  
+  @State private var budget = Budget(
+    name: "", 
+    amount: 0,
+    startDate: .now,
+    endDate: .now.addingTimeInterval(30*24*60*60),
+    expenses: [])
+  
+  var body: some View {
+    NavigationView {
+      Form {
+        Section("Name") {
+          TextField("E.g. Jan daily budget", text: $budget.name)
+        }
+        
+        Section("Budget period") {
+          DatePicker("First day", selection: $budget.startDate, displayedComponents: [.date])
+          DatePicker("Last day", selection: $budget.endDate, displayedComponents: [.date])
+        }
+        
+        Section {
+          TextField(
+            "Amount",
+            value: $budget.amount,
+            format: .number
+          )
+          
+          LabeledContent {
+            Text("\(budget.dailyAmount, specifier: "%.2f")")
+          } label: {
+            HStack {
+              Image(systemName: "info.circle")
+              Text("Daily budget")
+            }
+          }
+          .foregroundStyle(.gray)
+        } header: {
+          Text("Amount")
+        } footer: {
+          Text("Total amount available for entire budget period")
+        }
+        
+        if isEditMode {
+          Button(role: .destructive, action: onDeleteTapped) {
+            HStack {
+              Image(systemName: "trash")
+              Text("Delete budget")
+            }
+          }
+          .frame(maxWidth: .infinity)
+        }
+      }
+      .navigationTitle(
+        isEditMode ? "Edit budget" : "Create budget"
+      )
+      .navigationBarTitleDisplayMode(.inline)
+      
+      .toolbar {
+        Button(action: onSaveTapped, label: {
+          Text("Save")
+        })
+      }
+    }
+  }
+  
+  private func onSaveTapped() {
+    onSave(budget)
+  }
+  
+  private func onDeleteTapped() {
+    onDelete()
+  }
+}
+
+#Preview {
+  EditBudget(.new)
+}
