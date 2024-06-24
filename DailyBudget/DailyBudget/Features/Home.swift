4a40203d77cf5913532185c84c13ba1d6370a06f
phlippieb <phlippie.bosman@gmail.com>
2024-06-24 10:26:28 +0200

    Add app info UI

--- DailyBudget/DailyBudget/Features/Home.swift
+++ DailyBudget/DailyBudget/Features/Home.swift
@@ -7,26 +7,56 @@ struct Home: View {
   @EnvironmentObject private var currentDate: CurrentDate
   
   @State private var editingBudget: BudgetModel??
+  @State private var showingAppInfo = true
   
   var body: some View {
     NavigationView {
-      Group {
-        if budgets.isEmpty {
-          Text("No budgets")
-            .foregroundStyle(.gray)
-        } else {
-          List {
-            ForEach(budgets) { budget in
-              NavigationLink {
-                Text("")
-                ViewBudget(budget: budget)
-              } label: {
-                BudgetListItem(item: budget)
+      ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+        Group {
+          if budgets.isEmpty {
+            Button("Add a budget") {
+              onAddBudget()
+            }
+          } else {
+            List {
+              ForEach(budgets) { budget in
+                NavigationLink {
+                  Text("")
+                  ViewBudget(budget: budget)
+                } label: {
+                  BudgetListItem(item: budget)
+                }
               }
             }
           }
         }
+        .frame(maxHeight: .infinity)
+        
+        Group {
+          if showingAppInfo {
+            Spacer()
+            VStack {
+              Link(destination: URL(string: "https://phlippieb.github.io/daily-budget-app/")!) {
+                HStack {
+                  Text("Daily Budget")
+                  Image(systemName: "info.circle")
+                }
+              }
+              Text("Created by Phlippie Bosman")
+                .font(.footnote)
+            }
+            .padding()
+            .background(
+              Material.regular
+            )
+            .transition(.offset(.init(width: 0, height: 100)))
+          }
+        }
+        .cornerRadius(20)
+        .padding()
+        
       }
+
       .navigationTitle("Budgets")
       .navigationBarTitleDisplayMode(.inline)
       
@@ -41,6 +71,14 @@ struct Home: View {
       .sheet(item: $editingBudget) { _ in
         EditBudget(budget: $editingBudget)
       }
+  
+      // MARK: Show/hide app info
+      .gesture(
+        DragGesture().onChanged { value in
+          showingAppInfo = (value.translation.height > 0)
+        }
+      )
+      .animation(.easeInOut, value: showingAppInfo)
     }
   }
   
@@ -52,6 +90,7 @@ struct Home: View {
 #Preview {
   let config = ModelConfiguration(isStoredInMemoryOnly: true)
   let container = try! ModelContainer(for: BudgetModel.self, configurations: config)
+  container.mainContext.insert(BudgetModel())
   
   return Home()
     .modelContainer(container)
