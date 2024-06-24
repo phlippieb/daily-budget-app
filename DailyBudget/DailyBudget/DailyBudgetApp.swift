89848e574d969a3670add6f425aab7fc8aa6d0be
phlippieb <phlippie.bosman@gmail.com>
2024-06-24 08:08:14 +0200

    Update current date on date change notification Resolves #23

--- DailyBudget/DailyBudget/DailyBudgetApp.swift
+++ DailyBudget/DailyBudget/DailyBudgetApp.swift
@@ -2,11 +2,22 @@ import SwiftUI
 
 @main
 struct DailyBudgetApp: App {
+  var currentDate = CurrentDate()
+  
   var body: some Scene {
     WindowGroup {
       Home()
+      
+      // MARK: Provide persistent model container
         .modelContainer(for: BudgetModel.self)
-        .environmentObject(CurrentDate())
+        
+      // MARK: Provide and update current date
+        .environmentObject(currentDate)
+        .onReceive(NotificationCenter.default.publisher(
+          for: Notification.Name.NSCalendarDayChanged
+        )) { _ in
+          currentDate.value = .now
+        }
     }
   }
 }
