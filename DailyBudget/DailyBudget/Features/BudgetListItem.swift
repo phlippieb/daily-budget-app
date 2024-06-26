365f04972c59bd3d78f1d40d9a9992b020233d5b
phlippieb <phlippie.bosman@gmail.com>
2024-06-26 08:49:14 +0200

    Fix unreadable amount for inactive budget in dark mode

--- DailyBudget/DailyBudget/Features/BudgetListItem.swift
+++ DailyBudget/DailyBudget/Features/BudgetListItem.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import SwiftData
 
 struct BudgetListItem: View {
   let item: BudgetModel
@@ -27,9 +28,7 @@ struct BudgetListItem: View {
           Text("\(info.currentAllowance, specifier: "%.2f")")
             .font(.title)
             .foregroundStyle(
-              info.currentAllowance > 0 ? .green
-              : info.currentAllowance < 0 ? .red
-              : .black
+              info.currentAllowance >= 0 ? .green : .red
             )
         } label: {
           Text("Available")
@@ -41,8 +40,7 @@ struct BudgetListItem: View {
         LabeledContent {
           Text("\(item.totalExpenses, specifier: "%.2f") / \(item.amount, specifier: "%.2f")")
             .foregroundStyle(
-              info.currentAllowance > 0 ? .black
-              : .red
+              info.budget.totalExpenses <= info.budget.amount ? Color(UIColor.label) : .red
             )
         } label: {
           Text("Budget spent")
@@ -53,7 +51,71 @@ struct BudgetListItem: View {
 }
 
 #Preview {
-  BudgetListItem(item: BudgetModel())
-    .modelContainer(for: ExpenseModel.self, inMemory: true)
-    .environmentObject(CurrentDate())
+  let config = ModelConfiguration(isStoredInMemoryOnly: true)
+  let container = try! ModelContainer(for: BudgetModel.self, configurations: config)
+  let items: [BudgetModel] = [
+    BudgetModel(
+      name: "Current under budget",
+      amount: 100,
+      firstDay: .today,
+      lastDay: .today.adding(days: 1),
+      expenses: []),
+    
+    BudgetModel(
+      name: "Current over budget",
+      amount: 100,
+      firstDay: .today,
+      lastDay: .today.adding(days: 1),
+      expenses: [
+        ExpenseModel(name: "", amount: 200, date: .now)
+      ]),
+    
+    BudgetModel(
+      name: "Past under budget",
+      amount: 100,
+      firstDay: .today.adding(days: -1),
+      lastDay: .today.adding(days: -1),
+      expenses: [
+        ExpenseModel(name: "", amount: 50, date: .now)
+      ]),
+    
+    BudgetModel(
+      name: "Past over budget",
+      amount: 100,
+      firstDay: .today.adding(days: -1),
+      lastDay: .today.adding(days: -1),
+      expenses: [
+        ExpenseModel(name: "", amount: 150, date: .now)
+      ]),
+    
+    BudgetModel(
+      name: "Future under budget",
+      amount: 100,
+      firstDay: .today.adding(days: 1),
+      lastDay: .today.adding(days: 1),
+      expenses: [
+        ExpenseModel(name: "", amount: 50, date: .now)
+      ]),
+    
+    BudgetModel(
+      name: "Future over budget",
+      amount: 100,
+      firstDay: .today.adding(days: 1),
+      lastDay: .today.adding(days: 1),
+      expenses: [
+        ExpenseModel(name: "", amount: 150, date: .now)
+      ]),
+  ]
+  
+  items.forEach {
+    container.mainContext.insert($0)
+  }
+  
+  return List {
+    ForEach(items) { item in
+      BudgetListItem(item: item)
+    }
+  }
+  .modelContainer(for: ExpenseModel.self, inMemory: true)
+  .environmentObject(CurrentDate())
 }
