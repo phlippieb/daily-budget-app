18735ddd94c88c857927e27aa229a28cf6fa8c83
phlippieb <phlippie.bosman@gmail.com>
2024-06-28 12:16:42 +0200

    Update models to conform to iCloud requirements

--- DailyBudget/DailyBudget/Models/BudgetModel.swift
+++ DailyBudget/DailyBudget/Models/BudgetModel.swift
@@ -7,11 +7,11 @@ import SwiftData
 /// An instance represents a purpose-specific, time-ranged budget and its associated expenses.
 /// This model is ignorant of the current date vis-a-vis the budget's date; see BudetAtDate
 @Model final class BudgetModel {
-  @Attribute(.allowsCloudEncryption) var name: String
-  @Attribute(.allowsCloudEncryption) var amount: Double
-  @Attribute(.allowsCloudEncryption) var startDate: Date
-  @Attribute(.allowsCloudEncryption) var endDate: Date
-  @Attribute(.allowsCloudEncryption) var expenses: [ExpenseModel]
+  @Attribute(.allowsCloudEncryption) var name: String = ""
+  @Attribute(.allowsCloudEncryption) var amount: Double = 0
+  @Attribute(.allowsCloudEncryption) var startDate: Date = Date()
+  @Attribute(.allowsCloudEncryption) var endDate: Date = Date()
+  @Attribute(.allowsCloudEncryption) var expenses: [ExpenseModel]? = []
   
   init(name: String, amount: Double, startDate: Date, endDate: Date, expenses: [ExpenseModel]) {
     self.name = name
@@ -53,7 +53,7 @@ extension BudgetModel {
   }
   
   var totalExpenses: Double {
-    expenses.reduce(0) { $0 + $1.amount }
+    expenses?.reduce(0) { $0 + $1.amount } ?? 0
   }
 }
 
