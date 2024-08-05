f95842db833970c79a3b02a6f25205e56eae9569
phlippieb <phlippie.bosman@gmail.com>
2024-08-05 14:28:46 +0200

    Combine negative cases for no active budget selected for SpentTodayWidgetView

--- DailyBudget/DailyBudgetWidget/Widgets/SpentTodayWidgetView.swift
+++ DailyBudget/DailyBudgetWidget/Widgets/SpentTodayWidgetView.swift
@@ -15,33 +15,29 @@ struct SpentTodayWidgetView: View {
   }
   
   var body: some View {
-    if let viewModel {
-      if let secondaryAmount = viewModel.secondaryAmount {
-        VStack(alignment: .center) {
-          AmountText(
-            amount: secondaryAmount,
-            wholePartFont: .system(
-              size: UIFont.preferredFont(
-                forTextStyle: .largeTitle).pointSize * 2)
-          )
-          .minimumScaleFactor(0.1)
+    if let viewModel, let secondaryAmount = viewModel.secondaryAmount {
+      VStack(alignment: .center) {
+        AmountText(
+          amount: secondaryAmount,
+          wholePartFont: .system(
+            size: UIFont.preferredFont(
+              forTextStyle: .largeTitle).pointSize * 2)
+        )
+        .minimumScaleFactor(0.1)
+        .bold()
+        
+        Text("Spent today")
+          .fontWeight(.light)
+        
+        Text(viewModel.name)
+          .font(.subheadline)
           .bold()
-          
-          Text("Spent today")
-            .fontWeight(.light)
-          
-          Text(viewModel.name)
-            .font(.subheadline)
-            .bold()
-            .lineLimit(2)
-            .multilineTextAlignment(.center)
-        }
-      } else {
-        Text("Select an active budget to see today's spending")
+          .lineLimit(2)
+          .multilineTextAlignment(.center)
       }
       
     } else {
-      Text("No budget selected").foregroundStyle(.gray)
+      Text("Select an active budget").foregroundStyle(.gray)
       Text("Long-press to configure")
         .font(.footnote)
     }
