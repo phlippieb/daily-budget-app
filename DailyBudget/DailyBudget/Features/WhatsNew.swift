37836ff4794818fba6b9274c1b60e8a4e16b2714
phlippieb <phlippie.bosman@gmail.com>
2024-08-08 10:07:52 +0200

    Update Whats New for 1.4

--- DailyBudget/DailyBudget/Features/WhatsNew.swift
+++ DailyBudget/DailyBudget/Features/WhatsNew.swift
@@ -5,9 +5,8 @@ struct WhatsNew: View {
   
   var body: some View {
     VStack(alignment: .leading) {
-      Spacer(minLength: 8)
       HStack(alignment: .lastTextBaseline) {
-          Text("New in version 1.4")
+        Text("New in version \(NotableUpdate.latest.version)")
           Image(systemName: "sparkles")
       }
       .font(.subheadline)
@@ -19,35 +18,56 @@ struct WhatsNew: View {
         ], startPoint: .leading, endPoint: .trailing)
       )
       
-      Text("Saving tips")
-        .font(.headline)
       
       Text("")
-      Text("See saving tips for your active budgets! These tips are based on your current status — if you're under budget, they'll tell you how much you can save for tomorrow; and if you're over budget, they'll tell you how many days it'll take to get back on track.")
+      Text("New widget: Spent Today").font(.headline)
+      Text("Designed to complement the \"Available Today\" widget, the new \"Spent Today\" widget shows you your total expenses for the day. Pro tip: combine these widgets in a stack!")
       
-      ZStack {
-        Tip(tip: .breakEven(days: 2))
+      SpentTodayWidgetView(entry: .init(date: .now, budgetToDisplay: .placeholder))
         .padding()
         .background()
         .overlay(Color.systemBackground.opacity(0.4))
         .cornerRadius(10)
         .shadow(radius: 10)
-        .rotationEffect(.degrees(-10))
-        .offset(x: 90, y: 70)
         .scaleEffect(0.8)
-        
-        Tip(tip: .availableTomorrow(amount: 255))
-        .padding()
-        .background()
-        .overlay(Color.systemBackground.opacity(0.3))
-        .cornerRadius(10)
-        .shadow(radius: 10)
-        .rotationEffect(.degrees(10))
-        .offset(x: -90, y: 20)
-        .scaleEffect(0.8)
-      }
       
-      Spacer(minLength: 80)
+      
+      Text("")
+      Text("Saving tips").font(.headline)
+      Text("See saving tips for your active budgets! These tips are based on your current status — if you're under budget, they'll tell you how much you can save for tomorrow; and if you're over budget, they'll tell you how many days it'll take to get back on track.")
+      
+      Tip(tip: .availableTomorrow(amount: 99))
+      .padding()
+      .background()
+      .overlay(Color.systemBackground.opacity(0.4))
+      .cornerRadius(10)
+      .shadow(radius: 10)
+      .scaleEffect(0.8)
+      
+      Tip(tip: .breakEven(days: 2))
+      .padding()
+      .background()
+      .overlay(Color.systemBackground.opacity(0.4))
+      .cornerRadius(10)
+      .shadow(radius: 10)
+      .scaleEffect(0.8)
+      
+      Text("")
+      Text("Improved editing screens").font(.headline)
+      Text("Creating and editing your budgets and expenses is just a little nicer now.")
+      Text("- Jump between text fields by using the arrows in the keyboard toolbar")
+      Text("- Toggle the quick entry setting while editing an expense to add another immediately after saving")
+      Text("- You can no longer pull down to dismiss if you changes, meaning you won't accidentally delete your hard work")
+      
+      Text("")
+      Text("Tapping on a widget now takes you to the corresponding budget")
+        .font(.headline)
+      Text("A small but important update. If you had the app open and are viewing a specific budget, and then tap on a widget set to track another budget, the app will now navigate to the budget for the widget you tapped on.")
+      
+      Text("")
+      Text("Start app on only active budget").font(.headline)
+      Text("Another small but nice update. If you only have one active budget, the app will open to that budget. You can always navigate back to the list of all budgets from there.")
+      
       HStack {
         Spacer()
         Button {
--- DailyBudget/DailyBudget/Models/NotableUpdate.swift
+++ DailyBudget/DailyBudget/Models/NotableUpdate.swift
@@ -1,11 +1,26 @@
 /// All app updates that introduced notable new features with associated "What's New" messages
 /// - NOTE: the rawValue **does not** correspond to the app version that introduced the feature
 enum NotableUpdate: Int, CaseIterable {
+  /// 1.3 adds widgets
   case widgets = 1
+  /// 1.4 adds:
+  /// - another widget (spent today)
+  /// - tapping on widget now goes to correct budget
+  /// - savings tips
+  /// - improved editing
+  /// - launch app on active budget
+  case tipsEtc
 }
 
 extension NotableUpdate {
   static var latest: Self {
     self.allCases.last!
+  }
+  
+  var version: String {
+    switch self {
+    case .widgets: return "1.3"
+    case .tipsEtc: return "1.4"
+    }
   }
 }
