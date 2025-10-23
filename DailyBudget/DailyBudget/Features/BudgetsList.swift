c4f710f466dabfd1753d6d295d9de34124a0b34b
Phlippie <phlippie.bosman@gmail.com>
2025-10-23 18:49:56 +0200

    Group budget list by year

--- DailyBudget/DailyBudget/Features/BudgetsList.swift
+++ DailyBudget/DailyBudget/Features/BudgetsList.swift
@@ -4,14 +4,25 @@ struct BudgetsList: View {
   let title: String
   let budgets: [BudgetModel]
   
+  var budgetsByYear: [(String, [BudgetModel])] {
+    Dictionary(grouping: budgets) { budget in
+      Calendar.current.component(.year, from: budget.startDate)
+    }
+    .sorted { $0.0 > $1.0 }
+    .map { (String($0.0), $0.1) }
+  }
   
   var body: some View {
       List {
-        ForEach(budgets) { budget in
-          NavigationLink {
-            ViewBudget(budget: budget)
-          } label: {
-            BudgetListItem(item: budget)
+        ForEach(budgetsByYear, id: \.0) { (year, budgets) in
+          Section(year) {
+            ForEach(budgets) { budget in
+              NavigationLink {
+                ViewBudget(budget: budget)
+              } label: {
+                BudgetListItem(item: budget)
+              }
+            }
           }
         }
       }
